a99cada9b6821a87c9e9f5703d2ce757dde46d1e
Zeta912 <ag.sebastian912@gmail.com>
2023-01-25 14:53:36 -0300

    Project Started

---
+++ to-dolist/src/App.js
@@ -0,0 +1,34 @@
+
+import './App.css';
+import {useState} from "react";
+
+function App () {
+  const [todoList, setTOdoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+  
+  const addTask = () => {
+    const newTodoList = [...todoList,newTask];
+    setTOdoList(newTodoList);
+  };
+
+  return (
+    <div className="App">
+      <div className="addTask">
+        <input onChange={handleChange} />
+        <button onClick={addTask}> Add Task </button>
+      </div>
+      <div className="list">
+      {todoList.map((task) => {
+        return <h1>{task}</h1>;
+      })}
+      </div>
+      
+      </div>
+  );
+}
+
+export default App;
